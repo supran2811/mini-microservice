8583fd034cafbd4c17e5739e6a62de9a6ea14c02
Supran Sengupta <supransengupta@gmail.com>
2020-06-26 07:35:36 +0530

    added event bus

---
+++ posts/index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", async (req, res) => {
+  const { title } = req.body;
+  const id = randomBytes(4).toString("hex");
+  posts[id] = {
+    id,
+    title,
+  };
+
+  await axios.post("http://localhost:4005/events", {
+    type: "PostCreated",
+    data: {
+      id,
+      title,
+    },
+  });
+
+  res.status(201).send(posts[id]);
+});
+
+app.post("/events", (req, res) => {
+  const { type } = req.body;
+  console.log("Event generated ", type);
+  res.send({});
+});
+
+app.listen(4000, () => {
+  console.log("Listening to port 4000");
+});
