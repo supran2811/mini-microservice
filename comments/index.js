be6cea8bb327b725654aaf5e4ce5d68f4520c009
Supran Sengupta <supransengupta@gmail.com>
2020-06-28 09:01:47 +0530

    updated

--- comments/index.js
+++ comments/index.js
@@ -23,20 +23,37 @@ app.post("/posts/:id/comments", async (req, res) => {
   commentsByPostId[req.params.id] = comments;
 
   await axios.post("http://localhost:4005/events", {
-    type: "CommendCreated",
+    type: "CommentCreated",
     data: {
       id: commentId,
       content,
       postId: req.params.id,
+      status: "pending",
     },
   });
 
   res.status(201).send(comments);
 });
 
-app.post("/events", (req, res) => {
-  const { type } = req.body;
-  console.log("Event generated ", type);
+app.post("/events", async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated") {
+    const { id, postId, status } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((item) => item.id === id);
+    comment.status = status;
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        postId,
+        status,
+        content: comment.content,
+      },
+    });
+  }
+
   res.send({});
 });
 
