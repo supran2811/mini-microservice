b1dd95bd7e327093e57c3299af0c102399629dbc
Supran Sengupta <supransengupta@gmail.com>
2020-07-04 20:50:06 +0530

    updated

--- event-bus/index.js
+++ event-bus/index.js
@@ -14,7 +14,7 @@ app.post("/events", (req, res) => {
 
   axios.post("http://posts-clusterip-srv:4000/events", event);
   axios.post("http://comments-srv:4001/events", event);
-  axios.post("http://query-clusterip-srv:4002/events", event);
+  axios.post("http://query-srv:4002/events", event);
   axios.post("http://moderation-srv:4003/events", event);
 
   res.send({ status: "OK" });
